7ccd1db061548ea4f1dfd5cb1861ad638e7c03cb
DianaLlamoca <89529542+DianaLlamoca@users.noreply.github.com>
2024-11-14 19:06:25 -0500

    Create data.ts

---
+++ Actividad14/src/graphql/data.ts
@@ -0,0 +1,25 @@
+//Para implementar las consultas, se agregarán datos
+//JSON estáticos
+export const db=[
+    {
+        zip:"96815",
+        weather:"sunny",
+        tempC:"25C",
+        tempF:"70F",
+        friends:["96814","96826"]
+    },
+    {
+        zip:"96826",
+        weather:"sunny",
+        tempC:"30C",
+        tempF:"86F",
+        friend:["96814","96814"]
+    },
+    {
+        zip:"96814",
+        weather:"sunny",
+        tempC:"20C",
+        tempF:"68F",
+        friends: ["96815","96826"]
+    }
+];
