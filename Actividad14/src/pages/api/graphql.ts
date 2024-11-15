0ce42ea51ba8c6571db5535385b47ceb2ff4004d
DianaLlamoca <89529542+DianaLlamoca@users.noreply.github.com>
2024-11-14 19:08:01 -0500

    Create graphql.ts

---
+++ Actividad14/src/pages/api/graphql.ts
@@ -0,0 +1,45 @@
+//Se importan los archivos necesarios para crear el endpoint de GraphQL
+//Esto incluye el esquema GraphQL, así como los resolvers, que se crearon anteriormente.
+import {ApolloServer} from "@apollo/server";
+import {startServerAndCreateNextHandler} from "@as-integrations/next";
+import {resolvers} from "../../graphql/resolvers";
+import {typeDefs} from "../../graphql/schema";
+import {NextApiHandler,NextApiRequest,NextApiResponse} from "next";
+//Se importa la función dbConnect desde el middleware
+import dbConnect from "../../../middleware/db-connect";
+
+//Se inicia el servidor
+//@ts-ignore
+const server=new ApolloServer({
+    resolvers,
+    typeDefs
+});
+
+//Se crea el manejador de la API
+const handler=startServerAndCreateNextHandler(server);
+
+//Se crea un wrapper o envoltorio para agregar los encabezados CORS
+const allowCors=
+(fn:NextApiHandler)=>async(req:NextApiRequest,res:NextApiResponse)=>{
+    res.setHeader("Allow","POST");
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Methods","POST");
+    res.setHeader("Access-Control-Allow-Headers","*");
+    res.setHeader("Access-Control-Allow-Credentials","true");
+
+    //Se devuelve 200 con los encabezados CORS.
+    if (req.method==="OPTIONS"){
+        res.status(200).end();
+    }
+    return await fn(req,res);
+};
+
+//Se crea un nuevo wrapper para asegurarnos de que cada llamada
+//a la API se conecte a la base de datos:
+const connectDB=(fn:NextApiHandler)=>
+    async (req:NextApiRequest,res:NextApiResponse)=>{
+        await dbConnect();
+        return await fn(req,res);
+    };
+
+export default connectDB(allowCors(handler));
