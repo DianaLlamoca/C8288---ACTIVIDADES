f07ee3fc8225cda03a96cf7df91ed918e6092f03
DianaLlamoca <89529542+DianaLlamoca@users.noreply.github.com>
2024-11-14 19:08:21 -0500

    Create [zipcode].ts

---
+++ Actividad14/src/pages/api/[zipcode].ts
@@ -0,0 +1,18 @@
+import type {NextApiRequest,NextApiResponse} from "next";
+import { findByZip } from "../../../mongoose/weather/services";
+import dbConnect from "../../../middleware/db-connect";
+dbConnect();
+
+type WeatherDetailType={
+    zipcode:string;
+    weather:string;
+    temp?:number;
+}
+
+export default async function handler(
+    req:NextApiRequest,
+    res:NextApiResponse
+):Promise<NextApiResponse<WeatherDetailType>|void>{
+    let data=await findByZip(req.query.zipcode as string);
+    return res.status(200).json(data);
+}
